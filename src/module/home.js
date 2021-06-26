d7a6b48e6b9bcddcd8d25fab4aed91363baf5c3f
anniewey <annie_wey@hotmail.com>
2021-06-26 18:07:05 +0800

    Configure react-navigation and create new screens

---
+++ src/module/home.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native';
+
+const Home = () => {
+    return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+    </View>
+    );
+}
+  
+  const styles = StyleSheet.create({
+    sectionContainer: {
+      marginTop: 32,
+      paddingHorizontal: 24,
+    },
+    sectionTitle: {
+      fontSize: 24,
+      fontWeight: '600',
+    },
+    sectionDescription: {
+      marginTop: 8,
+      fontSize: 18,
+      fontWeight: '400',
+    },
+    highlight: {
+      fontWeight: '700',
+    },
+  });
+  
+  export default Home;
